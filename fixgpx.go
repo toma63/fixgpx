5b7fc1b7474993eaf9ee365e4ef951efb8064a42
toma <t_arns@yahoo.com>
2017-05-28 17:48:42 -0700

    added LoadGPXIn GetTimeDelta and tests

--- fixgpx.go
+++ fixgpx.go
@@ -3,14 +3,19 @@ package fixgpx
 import (
 	"fmt"
 	"time"
-	//"os"
+	"os"
 	"regexp"
 	//"strings"
-	//"bufio"
+	"bufio"
 )
 
 // create parsing regexps
-var timeTagRe = regexp.MustCompile(`<time>(.+)<\/time>`)
+var timeTagRE = regexp.MustCompile(`<time>(.+)<\/time>`)
+var metaStartTagRE = regexp.MustCompile(`^\s*<metadata>`)
+var metaEndTagRE = regexp.MustCompile(`^\s*</metadata>`)
+var trkStartTagRE = regexp.MustCompile(`^\s*<trk>`)
+var trkEndTagRE = regexp.MustCompile(`^\s*</trk>`)
+	
 
 // translate an iso-8601 utc time to unix epoch int64
 //  looks like 2006-01-02T15:04:05.000Z
@@ -34,3 +39,89 @@ func epoch2iso(epoch int64) string {
 	return iso + "Z" // gpx includes a trailing Z for zulu (utc)
 }
 
+// load the entire input file into a string slice
+// allow multiple passes
+func loadGPXIn(gpxin string) ([]string, error) {
+
+	// buffer for lines in the file
+	lineBuf := []string{}
+
+	// open the gpx file
+	fdin, finerr := os.Open(gpxin)
+	if finerr != nil {
+		return lineBuf, fmt.Errorf("fixgpx: Error opening gpx file %s: %v\n", gpxin, finerr)
+	}
+	defer fdin.Close()
+
+	// read the file into a slice
+	scanner := bufio.NewScanner(fdin)
+
+	for scanner.Scan() {
+		lineBuf = append(lineBuf, scanner.Text())
+	}
+
+	return lineBuf, nil
+}
+
+// get the time delta
+// The time in the first trkpt should match the time in the metadata tag
+// return the delta between the metadata tag and the first trkpt as a unix epoch int64
+func GetTimeDelta(lines []string) (int64, error) {
+
+	// result variables
+	metaEpoch := int64(0)
+	firstTrkEpoch := int64(0)
+
+	//state variables
+	inMeta := false
+	inTrk := false
+
+	for _, line := range lines {
+
+		// entering metadata tag
+		if metaStartRm := metaStartTagRE.FindStringSubmatch(line) ; metaStartRm != nil {
+			inMeta = true
+		}
+
+		// leaving metadata tag
+		if metaEndRm := metaEndTagRE.FindStringSubmatch(line) ; metaEndRm != nil {
+			inMeta = false
+		}
+
+		// entering trk tag
+		if trkStartRm := trkStartTagRE.FindStringSubmatch(line) ; trkStartRm != nil {
+			inTrk = true
+		}
+
+		// leaving trk tag
+		if trkEndRm := trkEndTagRE.FindStringSubmatch(line) ; trkEndRm != nil {
+			inTrk = false
+		}
+
+		// capture time tags
+		if timeTagRm := timeTagRE.FindStringSubmatch(line) ; timeTagRm != nil {
+			epoch, err := iso2epoch(timeTagRm[1])
+			if err != nil {
+				return 0, fmt.Errorf("GetTimeDelta: Error: %v", err)
+			}
+
+			if inMeta {
+				metaEpoch = epoch
+			} else if inTrk {
+				firstTrkEpoch = epoch
+
+				// just need the first one
+				if metaEpoch != 0 {
+					return firstTrkEpoch - metaEpoch, nil
+				} else {
+					return 0, fmt.Errorf("GetTimeDelta: Error: trk time found while 0 meta epoch")
+				}
+
+			} else {
+				return 0, fmt.Errorf("GetTimeDelta: Error: time tag in unexpected context")
+			}
+		}
+	}
+	// return to keep the compiler happy, shouldn't get here
+	return 0, fmt.Errorf("getTimeDelta: Error: reached final return and shouldn't have")
+}
--- fixgpx_test.go
+++ fixgpx_test.go
@@ -29,3 +29,61 @@ func TestIso2epoch(t *testing.T) {
 	}
 
 }
+
+func TestLoadGPXIn(t *testing.T) {
+
+	testFile := "./testdata/sample.gpx"
+
+	lineBufUT, err := loadGPXIn(testFile)
+	if err != nil {
+		t.Errorf("LoadGPXIn returned an unexpected error opening %s: %v", testFile, err)
+	}
+
+	// check an assortment of line including first and last
+	// map of line#, expected string
+	testLines := map[int]string {
+		0: `<?xml version="1.0" encoding="UTF-8"?>`,
+		len(lineBufUT) - 1: "</gpx>",
+		10: "    <time>2017-02-22T21:28:02.000Z</time>",
+		96: `      <trkpt lat="37.401199601590633392333984375" lon="-121.93786279298365116119384765625">`,
+		5411: "            <ns3:hr>146</ns3:hr>",
+	}
+
+	for idx, expected := range testLines {
+		if lineBufUT[idx] != expected {
+			t.Errorf("LoadGPXIn line %d does not match expected value: %s, actual: %s", idx, expected, lineBufUT[idx])
+		}
+	}
+	
+}
+
+func TestGetTimeDelta(t *testing.T) {
+
+	testLines := []string{"wozzle",
+		"gozzle snozzle\n",
+		"<sddad>schmoo<dsaUTYTY>",
+		"     metadata",
+		" <metadata>",
+		"bazzle",
+		"    <time>2017-02-22T21:28:02.000Z</time>",
+		"</metadata>",
+		"sdasdad",
+		"<><><><>",
+		"<trk>",
+		"        <time>2017-03-01T23:15:45.000Z</time>",
+		"</trk>",
+		`<trkpt lat="37.40106205455958843231201171875" lon="-121.93716533482074737548828125">`,
+	}
+
+	goldenDelta := int64(611263)
+
+	res, err := GetTimeDelta(testLines)
+
+	if err != nil {
+		t.Errorf("GetTimeDelta returned an unexpected error: %v", err)
+	}
+
+	if res != goldenDelta {
+		t.Errorf("getTimeDelta return %d instead of %d as expected", res, goldenDelta)
+	}
+}
